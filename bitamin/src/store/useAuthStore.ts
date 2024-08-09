80b098d325e0d994d241964e6ebc16ad85b79597
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-10 01:24:19 +0900

    refreshToken, accessToken 보안강화

--- bitamin/src/store/useAuthStore.ts
+++ bitamin/src/store/useAuthStore.ts
@@ -11,26 +11,43 @@ interface AuthState {
   clearAuth: () => void
 }
 
-function base64Decode(str: string): string {
+function base64UrlDecode(str: string): string {
   try {
+    // Base64URL 특수 문자 대체
+    str = str.replace(/-/g, '+').replace(/_/g, '/')
+    // Base64 디코딩을 위한 패딩 추가
+    const pad = str.length % 4
+    if (pad) {
+      if (pad === 1) {
+        throw new Error('Invalid Base64URL string')
+      }
+      str += new Array(5 - pad).join('=')
+    }
     return decodeURIComponent(
       atob(str)
         .split('')
-        .map((c) => {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-        })
+        .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
         .join('')
     )
   } catch (e) {
-    console.error('Base64 decoding failed', e)
+    console.error('Base64URL decoding failed', e)
     return ''
   }
 }
 
 function parseJwt(token: string): any {
   try {
-    const base64Payload = token.split('.')[1]
-    const payload = base64Decode(base64Payload)
+    const parts = token.split('.')
+    if (parts.length !== 3) {
+      throw new Error('Invalid JWT format')
+    }
+
+    console.log('JWT parts:', parts)
+
+    const base64Payload = parts[1]
+    const payload = base64UrlDecode(base64Payload)
+    console.log('Decoded Payload:', payload)
+
     return JSON.parse(payload)
   } catch (e) {
     console.error('JWT parsing failed', e)
@@ -54,6 +71,8 @@ const useAuthStore = create(
       },
       setRefreshToken: (token) => set({ refreshToken: token }),
       setRole: (role) => set({ role }),
+
+      // 모두 null로 설정해서 사용자 로그아웃 상태로 변경
       clearAuth: () =>
         set({ accessToken: null, refreshToken: null, role: null }),
     }),
