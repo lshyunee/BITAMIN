ecf9fd69f6ba3f91690313c7bb39131d4f04aa66
최승탁 <tmdxkr5@hanmail.net>
2024-08-14 16:54:54 +0900

    merge

--- bitamin/src/stories/organisms/common/Header.tsx
+++ bitamin/src/stories/organisms/common/Header.tsx
@@ -12,7 +12,6 @@ const Header: FunctionComponent = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false)
   const { user, fetchUser, loading, clearUserData } = useUserStore()
 
-  // 유저 데이터를 강제로 불러오도록 수정
   useEffect(() => {
     fetchUser() // 항상 최신 유저 데이터를 불러오도록 수정
   }, [fetchUser])
@@ -159,12 +158,16 @@ const Header: FunctionComponent = () => {
               >
                 <div className="h-[1.563rem] flex flex-row items-center justify-center">
                   <div className="w-[5rem] relative flex items-center h-[1.5rem] shrink-0">
-                    <span className="w-full">
-                      <span className="text-[1.2rem]">{user?.name}</span>
-                      <span className="font-nanumbarunpen text-[1rem]">
-                        <span className="text-[1rem]">님</span>
+                    {loading ? (
+                      <div>로딩 중...</div>
+                    ) : (
+                      <span className="w-full">
+                        <span className="text-[1.2rem]">{user?.name}</span>
+                        <span className="font-nanumbarunpen text-[1rem]">
+                          <span className="text-[1rem]">님</span>
+                        </span>
                       </span>
-                    </span>
+                    )}
                   </div>
                 </div>
                 <div className="w-[0.563rem] h-[1.188rem] flex flex-col items-center justify-end py-[0.312rem] px-[0rem] box-border">
@@ -223,109 +226,6 @@ const Header: FunctionComponent = () => {
           </div>
         </div>
       )}
-            <div className="w-[3.125rem] relative rounded-sm bg-brand-sub h-[0.125rem]" />
-          </div>
-          {role === 'admin' && (
-            <div
-              className="w-[3.75rem] h-[4.625rem] flex flex-col items-center justify-center p-[0.312rem] box-border gap-[0.312rem] cursor-pointer"
-              onClick={onAdminClick}
-            >
-              <div className="w-[4.563rem] h-[1.438rem] flex flex-row items-end justify-center">
-                <div className="relative">관리자</div>
-              </div>
-              <div className="w-[3.125rem] relative rounded-sm bg-brand-sub h-[0.125rem]" />
-            </div>
-          )}
-        </div>
-        {accessToken ? (
-          <div className="w-[7.75rem] h-[6.125rem] flex flex-col items-start justify-end text-darkgray">
-            <div className="self-stretch flex flex-row items-center justify-start gap-[0.375rem]">
-              <div className="flex flex-row items-center justify-start gap-[0.437rem]">
-                <img
-                  className="w-[1.188rem] relative h-[1.188rem] overflow-hidden shrink-0"
-                  alt=""
-                  src="PersonCircle.svg"
-                />
-                <div
-                  className="w-[4.5rem] h-[1.188rem] flex flex-row items-center justify-start gap-[0.25rem] cursor-pointer"
-                  onClick={toggleDropdown}
-                >
-                  <div className="h-[1.563rem] flex flex-row items-center justify-center">
-                    {loading ? (
-                      <div>로딩 중...</div>
-                    ) : (
-                      <div className="w-[3.313rem] relative flex items-center h-[1.5rem] shrink-0">
-                        <span className="w-full">
-                          <span>{user?.name}</span>
-                          <span className="font-nanumbarunpen text-[1.063rem]">
-                            <span>{` `}</span>
-                            <span className="text-[0.75rem]">님</span>
-                          </span>
-                        </span>
-                      </div>
-                    )}
-                  </div>
-                  <div className="w-[0.563rem] h-[1.188rem] flex flex-col items-center justify-end py-[0.312rem] px-[0rem] box-border">
-                    <img
-                      className="w-[0.563rem] relative h-[0.375rem]"
-                      alt=""
-                      src="Vector.svg"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div
-                className="h-[1.5rem] flex flex-row items-start justify-end cursor-pointer"
-                onClick={onMessageClick}
-              >
-                <img
-                  className="w-[1.188rem] relative h-[1.231rem]"
-                  alt=""
-                  src="쪽지 버튼.svg"
-                />
-              </div>
-            </div>
-            {dropdownVisible && (
-              <div className="w-[5.75rem] shadow-[4px_4px_25px_rgba(0,_0,_0,_0.25)] flex flex-col items-start justify-start mt-[-0.125rem] text-[0.563rem] text-gray">
-                <div
-                  className="self-stretch bg-white h-[1.188rem] flex flex-col items-start justify-center py-[0.375rem] px-[1.5rem] box-border cursor-pointer"
-                  onClick={() => navigate('/mypage')}
-                >
-                  <div className="w-[2.688rem] flex flex-col items-center justify-start">
-                    <div className="relative">마이페이지</div>
-                  </div>
-                </div>
-                <div
-                  className="self-stretch rounded-t-none rounded-b-8xs bg-white h-[1.25rem] flex flex-col items-start justify-center py-[0.375rem] px-[1.5rem] box-border cursor-pointer"
-                  onClick={onLogoutClick}
-                >
-                  <div className="w-[2.688rem] flex flex-col items-center justify-start">
-                    <div className="self-stretch relative">로그아웃</div>
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="w-[7.75rem] h-[6.125rem] flex flex-row items-center justify-center gap-[0.187rem] text-dimgray">
-            <div
-              className="w-[3.125rem] relative flex items-center justify-center h-[2rem] shrink-0 cursor-pointer"
-              onClick={onLoginTextClick}
-            >
-              로그인
-            </div>
-            <div className="w-[1.25rem] relative text-black flex items-center justify-center h-[2rem] shrink-0">
-              /
-            </div>
-            <div
-              className="relative cursor-pointer"
-              onClick={onSignupTextClick}
-            >
-              회원가입
-            </div>
-          </div>
-        )}
-      </div>
     </div>
   )
 }
