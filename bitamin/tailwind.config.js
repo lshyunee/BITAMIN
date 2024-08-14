dccbbedd02e0367c95b4c0a8bebeedf9a59863fb
최승탁 <tmdxkr5@hanmail.net>
2024-08-14 10:55:59 +0900

    design: 디자인 추가

--- bitamin/tailwind.config.js
+++ bitamin/tailwind.config.js
@@ -7,8 +7,31 @@ module.exports = {
         bagel: ['"Bagel Fat One"', 'cursive'],
         nanum: ['"Nanum Gothic"', 'sans-serif'],
         ownglyph: ['"Ownglyph ryuttung"', 'cursive'],
+        'bagel-fat-one': "'Bagel Fat One'",
       },
     },
   },
   plugins: [],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         coral: '#ff713c',
+//       },
+//       spacing: {},
+//       fontFamily: {
+//         'bagel-fat-one': "'Bagel Fat One'",
+//       },
+//     },
+//     fontSize: {
+//       inherit: 'inherit',
+//     },
+//   },
+//   corePlugins: {
+//     preflight: false,
+//   },
+// }
