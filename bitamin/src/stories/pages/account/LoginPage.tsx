629f5e130db0a4b8d1d01e0adf81ebe07dc14553
최승탁 <tmdxkr5@hanmail.net>
2024-08-14 13:36:53 +0900

    refactor: refactor: 빌드 에러 수정 2차

--- bitamin/src/stories/pages/account/LoginPage.tsx
+++ bitamin/src/stories/pages/account/LoginPage.tsx
@@ -1,12 +1,10 @@
 import { useState, useCallback } from 'react'
-import axiosInstance, { setAccessToken } from 'api/axiosInstance' // 경로 수정
-import useAuthStore from 'store/useAuthStore' // 경로 수정
+import axiosInstance, { setAccessToken } from 'api/axiosInstance'
+import useAuthStore from 'store/useAuthStore'
 import { useCookies } from 'react-cookie'
 import { useNavigate } from 'react-router-dom'
 import styles from 'styles/account/LoginPage.module.css'
-import HeaderBeforeLogin from '@/stories/organisms/common/HeaderBeforeLogin'
 
-// 서버 응답 타입을 정의
 interface LoginResponse {
   accessToken: string
   refreshToken: string
@@ -15,7 +13,7 @@ interface LoginResponse {
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [, setCookie] = useCookies(['refreshToken']) // `cookies` 대신 `_`를 사용
+  const [, setCookie] = useCookies(['refreshToken'])
   const navigate = useNavigate()
 
   const {
@@ -36,17 +34,16 @@ const LoginPage: React.FC = () => {
       console.log('Access Token:', accessToken) // 토큰 확인
       console.log('Refresh Token:', refreshToken)
 
-      setAccessToken(accessToken) // axiosInstance에 accessToken 설정
-      setAuthAccessToken(accessToken) // zustand 상태 관리에 accessToken 설정
-      setAuthRefreshToken(refreshToken) // zustand 상태 관리에 refreshToken 설정
+      setAccessToken(accessToken)
+      setAuthAccessToken(accessToken)
+      setAuthRefreshToken(refreshToken)
 
       setCookie('refreshToken', refreshToken, {
         path: '/',
         secure: true,
-        sameSite: 'strict', // 또는 'lax' 또는 'none'으로 설정
+        sameSite: 'strict',
       })
 
-      // 세션 스토리지에 인증 상태 저장
       sessionStorage.setItem('isAuthenticated', 'true')
       alert('Login successful!')
       navigate('/home')
@@ -63,82 +60,80 @@ const LoginPage: React.FC = () => {
   }, [navigate])
 
   return (
-    <>
-      <div className={styles.div}>
-        <div className={styles.backred} />
-        <div className={styles.component66}>
-          <div className={styles.component63}>
-            <b className={styles.b}>로그인</b>
-            <b className={styles.bitamin1}>BItAMin에 오신 것을 환영합니다.</b>
-          </div>
-          <div className={styles.component65}>
-            <div className={styles.component65Child} />
-            <div className={styles.component64}>
-              <div className={styles.component61}>
-                <div className={styles.component61Child} />
-                <br />
-                <input
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="이메일을 입력하세요"
-                  className={styles.div11}
-                />
+    <div className={styles.div}>
+      <div className={styles.backred} />
+      <div className={styles.component66}>
+        <div className={styles.component63}>
+          <b className={styles.b}>로그인</b>
+          <b className={styles.bitamin1}>BItAMin에 오신 것을 환영합니다.</b>
+        </div>
+        <div className={styles.component65}>
+          <div className={styles.component65Child} />
+          <div className={styles.component64}>
+            <div className={styles.component61}>
+              <div className={styles.component61Child} />
+              <br />
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="이메일을 입력하세요"
+                className={styles.div11}
+              />
+            </div>
+            <div className={styles.component62}>
+              <div className={styles.component61Child} />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호를 입력하세요"
+                className={styles.div11}
+              />
+            </div>
+            <div className={styles.component55} onClick={handleLogin}>
+              <div className={styles.component55Child}>
+                <b className={styles.b1}>로그인</b>
               </div>
-              <div className={styles.component62}>
-                <div className={styles.component61Child} />
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="비밀번호를 입력하세요"
-                  className={styles.div11}
-                />
+            </div>
+            <div className={styles.component56} onClick={handleSignUp}>
+              <div className={styles.component56Child}>
+                <b className={styles.b2}>회원가입</b>
               </div>
-              <div className={styles.component55} onClick={handleLogin}>
-                <div className={styles.component55Child}>
-                  <b className={styles.b1}>로그인</b>
-                </div>
-              </div>
-              <div className={styles.component56} onClick={handleSignUp}>
-                <div className={styles.component56Child}>
-                  <b className={styles.b2}>회원가입</b>
-                </div>
-              </div>
+            </div>
 
-              <div className={styles.component60}>
-                <div className={styles.component60Child} />
-                <div className={styles.component60Item} />
-                <div className={styles.component58}>
-                  <img
-                    className={styles.reactIconssisinaver}
-                    alt=""
-                    src="react-icons/si/SiNaver.svg"
-                  />
-                  <div className={styles.div13}>네이버 로그인</div>
-                </div>
-                <div className={styles.component59}>
-                  <img
-                    className={styles.reactIconsfcfcgoogle}
-                    alt=""
-                    src="react-icons/fc/FcGoogle.svg"
-                  />
-                  <div className={styles.div13}>구글 로그인</div>
-                </div>
-                <div className={styles.component57}>
-                  <img
-                    className={styles.component57Child}
-                    alt=""
-                    src="Frame 1364.png"
-                  />
-                  <div className={styles.div15}>카카오 로그인</div>
-                </div>
+            <div className={styles.component60}>
+              <div className={styles.component60Child} />
+              <div className={styles.component60Item} />
+              <div className={styles.component58}>
+                <img
+                  className={styles.reactIconssisinaver}
+                  alt=""
+                  src="react-icons/si/SiNaver.svg"
+                />
+                <div className={styles.div13}>네이버 로그인</div>
+              </div>
+              <div className={styles.component59}>
+                <img
+                  className={styles.reactIconsfcfcgoogle}
+                  alt=""
+                  src="react-icons/fc/FcGoogle.svg"
+                />
+                <div className={styles.div13}>구글 로그인</div>
+              </div>
+              <div className={styles.component57}>
+                <img
+                  className={styles.component57Child}
+                  alt=""
+                  src="Frame 1364.png"
+                />
+                <div className={styles.div15}>카카오 로그인</div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
