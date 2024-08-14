30e37707b4787be8e543cfa293d723d65721547a
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-08-15 08:07:57 +0900

    마이페이지 css 수정

--- bitamin/src/stories/pages/account/ExLogin.tsx
+++ bitamin/src/stories/pages/account/ExLogin.tsx
@@ -1,62 +1,34 @@
-import { useState } from 'react'
-import axiosInstance, { setAccessToken } from 'api/axiosInstance'
-import useAuthStore from 'store/useAuthStore'
-import { useCookies } from 'react-cookie'
-import useUserStore from 'store/useUserStore' // useUserStore 임포트
+import { useEffect, useState } from 'react'
+import { getPhrases } from '@/api/phraseAPI'
+import styles from 'styles/main/MainPage.module.css'
+import mainImg from 'assets/image/mainImg.png'
 
 const ExLogin: React.FC = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [, setCookie] = useCookies(['refreshToken']) // `cookies` 대신 `_`를 사용
-  const setAuthAccessToken = useAuthStore((state) => state.setAccessToken)
-  const setAuthRefreshToken = useAuthStore((state) => state.setRefreshToken)
-  const fetchUser = useUserStore((state) => state.fetchUser) // fetchUser 가져오기
+  const [phraseContent, setPhraseContent] = useState('') // phraseContent 상태 추가
 
-  const handleLogin = async () => {
-    try {
-      const response = await axiosInstance.post('/auth/login', {
-        email,
-        password,
-      })
-
-      const { accessToken, refreshToken } = response.data
-      setAccessToken(accessToken)
-      setAuthAccessToken(accessToken)
-      setAuthRefreshToken(refreshToken)
-      setCookie('refreshToken', refreshToken, {
-        path: '/',
-        secure: true,
-        sameSite: 'strict',
-      })
-
-      console.log('Before fetchUser') // 여기까지 도달했는지 확인
-      const data = await fetchUser()
-      console.log('Fetched User Data:', data) // 유저 데이터가 제대로 출력되는지 확인
-
-      alert('Login successful!')
-    } catch (error: any) {
-      const errorMessage =
-        error.response?.data?.message || error.message || 'Login failed'
-      alert(`Login failed: ${errorMessage}`)
-      console.error('Login error:', error)
+  useEffect(() => {
+    const fetchPhrase = async () => {
+      try {
+        const data = await getPhrases()
+        setPhraseContent(data.phrase)
+      } catch (error) {
+        console.error('Error fetching the phrase:', error)
+      }
     }
-  }
+
+    fetchPhrase()
+  }, []) // 컴포넌트가 마운트될 때 한번만 호출
 
   return (
     <div>
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button onClick={handleLogin}>Login</button>
+      <div className={styles.innerSection}>
+        <img className={styles.mainImg} alt="Main Image" src={mainImg} />
+        <div className={styles.inner}>
+          <div className={styles.div3}>
+            <p className={styles.p}>{phraseContent}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
