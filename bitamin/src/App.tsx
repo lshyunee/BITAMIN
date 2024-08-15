557157e06c4d4f392a6a99c43e3c66aad5f024ae
최승탁 <tmdxkr5@hanmail.net>
2024-08-15 12:06:55 +0900

    design: 메인페이지 전체화면

--- bitamin/src/App.tsx
+++ bitamin/src/App.tsx
@@ -8,11 +8,16 @@ const AppContent: React.FC = () => {
   const location = useLocation()
   const excludeHeaderPaths = ['/', '/consult']
   const excludeFooterPaths = ['/', '/modal', '/consult', '/healthup']
+  const includeContentPaths = ['/'] // 특정 경로들을 여기에 추가하세요.
+
+  const contentClassName = includeContentPaths.includes(location.pathname)
+    ? 'flex-grow mt-[6%]'
+    : 'flex-grow' // 기본적으로는 'flex-grow'만 적용
 
   return (
     <div className="flex flex-col min-h-screen">
       {!excludeHeaderPaths.includes(location.pathname) && <Header />}
-      <div className="flex-grow mt-[6%]">
+      <div className="contentClassName">
         <AppRouter />
       </div>
       {!excludeFooterPaths.includes(location.pathname) && <Footer />}
